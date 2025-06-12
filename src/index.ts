aebd8473f293f37c513fd6e877290d55fa96ad8b
misdinfiyana09 <misdinfiyana09@gmail.com>
2025-06-12 11:14:57 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ export default {
 		}
 		// Only GET requests work with this proxy.
 		if (request.method !== "GET") return MethodNotAllowed(request);
-		return fetch(`https://lightblue-flamingo-273678.hostingersite.com`);
+		return fetch(`https://imajiner.id/`);
 	},
 } satisfies ExportedHandler;
